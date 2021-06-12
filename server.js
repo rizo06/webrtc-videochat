71238930e9686e47d746ab150af5f08b6a1fd19b
Shoazimov <rizo06@github.com>
2021-06-12 18:18:39 +0500

    Everything is done(I made Zoom Clone using SocketIo, ReactJS and WebRTC)

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server, {
+   cors: {
+      origin: "https://shoazimov:3000",
+      methods: ["GET", "POST", "DELETE"]
+   }
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callExcepted", data.signal);
+  });
+});
+
+server.listen(5000, () => console.log("Server is running on port 5000"));
